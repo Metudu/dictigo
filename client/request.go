778eefe95ca53d7c247eeb9cbeeb1d46d65f86ef
Metudu <tarikucar@gmail.com>
2024-04-21 00:54:53 +0300

    go fmt

--- client/request.go
+++ client/request.go
@@ -9,7 +9,7 @@ import (
 )
 
 type TranslationRequest struct {
-	Text       []string   `json:"text"`
+	Text       []string `json:"text"`
 	TargetLang string   `json:"target_lang"`
 }
 
@@ -22,14 +22,18 @@ type TranslationResponse struct {
 
 func SendRequest(language string, text []string) {
 	// Control if the user has the API key
-	if os.Getenv("DeepL-API-KEY") == "" {
+	var ApiKey string
+
+	if os.Getenv("DeepL_API_KEY") == "" {
 		fmt.Println("DeepL API key is not found. Consider to take a look at the GitHub repository of dictigo.")
 		os.Exit(1)
+	} else {
+		ApiKey = os.Getenv("DeepL_API_KEY")
 	}
 
 	url := "https://api-free.deepl.com/v2/translate"
 	reqBody := TranslationRequest{
-		Text: text,
+		Text:       text,
 		TargetLang: language,
 	}
 
@@ -45,7 +49,7 @@ func SendRequest(language string, text []string) {
 		return
 	}
 
-	req.Header.Set("Authorization", fmt.Sprintf(`DeepL-Auth-Key %s`,os.Getenv("DeepL-API-KEY")))
+	req.Header.Set("Authorization", "DeepL-Auth-Key "+ApiKey)
 	req.Header.Set("Content-Type", "application/json")
 
 	resp, err := client.Do(req)
@@ -56,13 +60,14 @@ func SendRequest(language string, text []string) {
 	defer resp.Body.Close()
 
 	var translationResponse TranslationResponse
+
 	if err := json.NewDecoder(resp.Body).Decode(&translationResponse); err != nil {
-		fmt.Println(err)
+		fmt.Println("Something went wrong when trying to decode response!")
 		return
 	}
 
 	fmt.Printf("Found %v translation(s)!\n", len(translationResponse.Translations))
 	for index, value := range translationResponse.Translations {
-		fmt.Printf("%v - %v\n", index + 1, value.Text)
+		fmt.Printf("%v - %v\n", index+1, value.Text)
 	}
 }
